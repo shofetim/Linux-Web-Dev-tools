3085bf5f1d3906c72959a22169c72b4dc3217cc3
Jordan Schatz <jordan@noionlabs.com>
2011-12-01 13:15:21 -0700

    Background assembler

--- presentation.rkt
+++ presentation.rkt
@@ -1,6 +1,46 @@
 #lang slideshow
-(require slideshow)
+(require slideshow
+         racket/draw)
 
+;; A pict to use behind the main content
+(define fade-bg
+  (let ([w (+ (* 2 margin) client-w)]
+        [h (+ (* 2 margin) client-h)]
+        [trans (make-object brush% "white" 'transparent)]
+        [inside (make-object brush% "white" 'solid)])
+    (inset (dc (lambda (dc x y)
+                 (let ([b (send dc get-brush)]
+                       [p (send dc get-pen)]
+                       [draw-one
+                        (lambda (i)
+                          (send dc draw-rectangle
+                                (+ x i) (+ y i)
+                                (- w (* 2 i)) (- h (* 2 i))))])
+                   (send dc set-brush trans)
+                   (color-series 
+                    dc margin 1
+                    (make-object color% "black")
+                    (make-object color% "white")
+                    draw-one
+                    #t #t)
+                   (send dc set-brush inside)
+                   (draw-one margin)
+                   (send dc set-pen p)
+                   (send dc set-brush b)))
+               w h 0 0)
+           (- margin))))
+
+;; Setup
+(current-slide-assembler
+  (lambda (s v-sep p)
+    (lt-superimpose fade-bg 
+         (apply vc-append v-sep
+                (if s
+                    (list (evenize-width (if (pict? s) s (titlet s))) p)
+                    (list p))))))
+
+
+;; Slides
 (slide
  (titlet "Linux Web Dev")
  (blank)
@@ -13,15 +53,18 @@
        "Download the presentation, code etc at:"
        "github.com/shofetim/Linux-Web-Dev-tools"))
 
+(slide
+ #:title "Why I develop on Linux"
+ (colorize (tt "\"If you want to learn to program, learn linux\"") "blue"))
 
 (slide
- #:title "More Paragraph Alignment"
- (frame
-  (para #:align 'center
-        "The" (tt "#:align 'center") "option for" (tt "para") "generates"
-        "a paragraph with centered lines of text"))
- (frame
-  (para "This line uses the" (tt "#:fill? #f") "option"))
- (para "The" (tt "#:fill? #f") "option creates a paragraph"
-            "that is wrapped to fit the slide, but it allows"
-            "the resulting pict to be more narrow than the slide"))
+ #:title "Do other people develop on linux?"
+ 'next (item "Websites deployed on linux")
+ 'next (item "languages/tools treat posix as the only first class citizen")
+ 'next (subitem "epoll/kqueue")
+ 'next (subitem "node.js")
+ 'next (item ".NET")
+ 'next (item "Remote administration / control")
+ 'next (item "Support from virtualization providers")
+ 'next (item "Universities that require unix knowledge")
+)
