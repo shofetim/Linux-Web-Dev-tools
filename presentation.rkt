26ec7a3f6819d4a4295282ed07272329fb1ceeb8
Jordan Schatz <jordan@noionlabs.com>
2011-12-01 08:09:37 -0700

    Presentation, readme, and generated pdf

---
+++ presentation.rkt
@@ -0,0 +1,18 @@
+#lang slideshow
+(require openssl/sha1 ;;Required for sha1 example
+         slideshow
+         slideshow/code) 
+
+(slide
+ #:title "Git 1 2 3!"
+ (para #:align 'center
+       "A fast introduction to git for professionals")
+ (blank)
+ (para #:align 'center
+       "Jordan Schatz"
+       (text "jordan@noionlabs.com" '(bold . modern) 25))
+ (blank)
+ (para #:align 'center
+       "You can fork this presentation at:"
+       "https://github.com/shofetim/git-1-2-3")
+ (blank))
